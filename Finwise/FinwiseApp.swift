b42024e10a20505bf133379fee47713fc4726919
goksualc <goksualcinkaya@gmail.com>
2025-04-11 21:17:28 -0400

    Initial Commit

---
+++ Finwise/FinwiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinwiseApp.swift
+//  Finwise
+//
+//  Created by Göksu Alçınkaya on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FinwiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
